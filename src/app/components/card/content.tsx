0f70241a5319e893895e345a742f3dc40980de23
fabricio.junior <fabriciojuniorsc@gmail.com>
2024-08-24 10:53:29 -0300

    Ajuste overflow conteúdo

--- src/app/components/card/content.tsx
+++ src/app/components/card/content.tsx
@@ -4,7 +4,7 @@ export default function CardContent({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="relative p-6 overflow-hidden flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
+        <div className="relative p-6 flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border">
             {children}
         </div>
     )
